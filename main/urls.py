dc748a6dbbfa68ecbe5863f39956a8349cb26439
Artem <artem.vashenko201010@gmail.com>
2024-09-30 21:28:18 +0300

    Об'єдання всіх гілок в master

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from .settings import main_app
 import home
 import tour
+import user
 
 home.home_app.add_url_rule(
     rule = "/",
@@ -10,5 +11,11 @@ tour.tour_app.add_url_rule(
     rule = "/tour/",
     view_func = tour.render_tour
 )
+user.user_app.add_url_rule(
+    rule = "/user/",
+    view_func = user.render_user
+)
 main_app.register_blueprint(blueprint = home.home_app)
 main_app.register_blueprint(blueprint = tour.tour_app)
+
+main_app.register_blueprint(blueprint = user.user_app)
---
+++ user/views.py
@@ -0,0 +1,6 @@
+import flask
+
+def render_user():
+    return flask.render_template(
+        template_name_or_list= "user.html"
+    )
